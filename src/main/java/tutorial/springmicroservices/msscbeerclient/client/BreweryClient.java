1ebbe3b750e4e29bf95ae6833d956ca93f943f76
Joseph Yacoub <joseph.yacoub@stud.h-da.de>
2022-04-07 00:19:18 +0200

    Adding Customer Consumption to the Client with simple Tests

--- src/main/java/tutorial/springmicroservices/msscbeerclient/client/BreweryClient.java
+++ src/main/java/tutorial/springmicroservices/msscbeerclient/client/BreweryClient.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import tutorial.springmicroservices.msscbeerclient.web.model.BeerDto;
+import tutorial.springmicroservices.msscbeerclient.web.model.CustomerDto;
 
 import java.net.URI;
 import java.util.UUID;
@@ -18,28 +19,47 @@ import java.util.UUID;
 @Data
 public class BreweryClient {
 
-public final String BEER_PATH_V1=   "/api/v1/beer/";
-private String apiHost;
+    public final String BEER_PATH_V1 = "/api/v1/beer/";
+    public final String CUSTOMER_PATH_V1 = "/api/v1/customer/";
+    private String apiHost;
 
-private final RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
-public BreweryClient(RestTemplateBuilder restTemplateBuilder){
-    this.restTemplate = restTemplateBuilder.build();
-}
+    public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
 
-public BeerDto getBeerById(UUID uuid){
-    return restTemplate.getForObject(apiHost + BEER_PATH_V1 + uuid.toString(), BeerDto.class);
-}
+    public BeerDto getBeerById(UUID uuid) {
+        return restTemplate.getForObject(apiHost + BEER_PATH_V1 + uuid.toString(), BeerDto.class);
+    }
 
-public URI saveNewBeer(BeerDto beerDto){
-    return restTemplate.postForLocation(apiHost + BEER_PATH_V1, beerDto);
-}
+    public URI saveNewBeer(BeerDto beerDto) {
+        return restTemplate.postForLocation(apiHost + BEER_PATH_V1, beerDto);
+    }
 
-public void updateBeer(UUID uuid, BeerDto beerDto){
-    restTemplate.put(apiHost + BEER_PATH_V1 +"/" + uuid.toString(), beerDto );
-}
+    public void updateBeer(UUID uuid, BeerDto beerDto) {
+        restTemplate.put(apiHost + BEER_PATH_V1 + "/" + uuid.toString(), beerDto);
+    }
 
-public void deleteBeer(UUID uuid){
-    restTemplate.delete(apiHost + BEER_PATH_V1 +"/"+ uuid.toString());
-}
+    public void deleteBeer(UUID uuid) {
+        restTemplate.delete(apiHost + BEER_PATH_V1 + "/" + uuid.toString());
+    }
+
+    /*Methods for the Customer*/
+
+    public CustomerDto getCustomerById(UUID uuid) {
+        return restTemplate.getForObject(apiHost + CUSTOMER_PATH_V1 + uuid.toString(), CustomerDto.class);
+    }
+
+    public URI saveNewCustomer(CustomerDto customerDto) {
+        return restTemplate.postForLocation(apiHost + getCUSTOMER_PATH_V1(), customerDto);
+    }
+
+    public void updateCustomer(UUID uuid, CustomerDto customerDto) {
+        restTemplate.put(apiHost + getCUSTOMER_PATH_V1() + uuid, customerDto);
+    }
+
+    public void deleteCustomer(UUID uuid) {
+        restTemplate.delete(apiHost + getCUSTOMER_PATH_V1() + uuid);
+    }
 }
--- src/test/java/tutorial/springmicroservices/msscbeerclient/client/BreweryClientTest.java
+++ src/test/java/tutorial/springmicroservices/msscbeerclient/client/BreweryClientTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import tutorial.springmicroservices.msscbeerclient.web.model.BeerDto;
+import tutorial.springmicroservices.msscbeerclient.web.model.CustomerDto;
 
 import java.net.URI;
 import java.util.UUID;
@@ -49,5 +50,41 @@ class BreweryClientTest {
     @Test
     void deleteBeer(){
         client.deleteBeer(UUID.randomUUID());
+    }
+
+    /* Test for the CustomerClient*/
+
+    @Test
+    void getCustomerById() {
+
+        CustomerDto customerDto= client.getCustomerById(UUID.randomUUID());
+
+        assertNotNull(customerDto);
+    }
+
+    @Test
+    void saveNewCustomer() {
+
+        CustomerDto customerDto = CustomerDto.builder()
+                .name("Me")
+                .build();
+
+        URI uri= client.saveNewCustomer(customerDto);
+
+        assertNotNull(uri);
+    }
+
+    @Test
+    void updateCustomer() {
+        CustomerDto customerDto = CustomerDto.builder()
+                .name("NewMe")
+                .build();
+
+        client.updateCustomer(UUID.randomUUID(),customerDto);
+    }
+
+    @Test
+    void deleteCustomer() {
+        client.deleteCustomer(UUID.randomUUID());
     }
 }
